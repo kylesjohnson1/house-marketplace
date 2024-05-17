24fd41f9bd0d095a3b106d66319ad07610fc0d00
Kyle Johnson <kylesjohnson1@gmail.com>
2024-05-17 14:21:18 -0500

    commit before vacation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import PrivateRoute from './components/PrivateRoute'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
+import Category from './pages/Category'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
