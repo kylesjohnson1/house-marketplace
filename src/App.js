606fb7f7c398dc09b397be6659edf16df342b223
Kyle Johnson <kylesjohnson1@gmail.com>
2024-07-28 18:03:43 -0500

    finished 106

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar'
 import Profile from './pages/Profile'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -27,6 +29,11 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
